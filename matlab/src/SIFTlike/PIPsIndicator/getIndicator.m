c9148912e9e9a65db17fbf6b97b2f319c9f352e8
Changfeng Liu <steven24liu@gmail.com>
2016-07-19 14:33:59 -0500

    rename getPIPS test script

---
+++ matlab/src/SIFTlike/PIPsIndicator/getIndicator.m
@@ -0,0 +1,8 @@
+function [ output_args ] = getIndicator( ts, PIPinfo )
+%ts: One time series sequence you want to compare, maybe after smoothing or not
+%PIPinfo: the information of PIPs
+
+
+
+end
+
