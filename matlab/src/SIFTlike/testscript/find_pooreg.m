ee78262121d91850769589e14bf092e001bca770
Changfeng Liu <steven24liu@gmail.com>
2016-07-28 16:46:48 -0500

    add find poor example for sc dataset

---
+++ matlab/src/SIFTlike/testscript/find_pooreg.m
@@ -0,0 +1,75 @@
+%find poor example of sc dataset 
+
+%clustering test
+addpath('../')
+clear;
+%clc;
+
+%% parameter
+WinLen=2;%sliding whindow length smooth 20
+PIPthr=0.15;
+
+%% similarity ranking parameters
+queryno=101;%401
+disp(['queryno=',num2str(queryno)])
+TopN2show=[3,5,20,50,100];
+topNaccu=100;%top N match accuracy
+
+%% load dataset
+%sc dataset
+load('/Users/Steven/Documents/GitHub/Frequent-Patterns-in-Time-Series/matlab/data/gt_sc.mat');
+load('/Users/Steven/Documents/GitHub/Frequent-Patterns-in-Time-Series/matlab/data/synthetic_control.mat');
+ts = synthetic_control;
+gt = gt_sc;
+[rnum,cnum]=size(ts);
+
+%% preprocessing
+%normalization/scaling
+ts_norm = ts;
+for i=1:rnum
+    ts_norm(i,:)=(ts(i,:)-mean(ts(i,:)))/ std(ts(i,:));
+    %ts_norm(i,:)=ts(i,:)/mean(ts(i,:));
+end
+
+%smoothing
+%WinLen=6;%sliding whindow length
+wts = [1/(2*WinLen);repmat(1/WinLen,WinLen-1,1);1/(2*WinLen)];
+for i=1:rnum
+    ts_smooth(i,:) = conv(ts_norm(i,:),wts,'valid');   
+end
+
+%% similarity ranking
+%topNaccu=50;%top N match accuracy
+query=ts(queryno,:);
+query_smooth=ts_smooth(queryno,:);
+
+% For time complexity: n TS, m D for each TS, x for PIP number; n*logn for finally ranking
+%%%%%PIPthr_dtw%%%%% - O(n*m + n*x^2 + n*logn)
+class=fix((queryno-1)/100)+1;
+[ ranking_PIPthr_dtw ] = SimRank_PIPthr_dtw( query_smooth,ts_smooth,PIPthr );
+interval=ranking_PIPthr_dtw(1:topNaccu);
+mismatch=interval(interval>(class*100) | interval<(class*100-99))
+
+%{
+%%%%%PIPthr_dtw2 only x,y%%%%% - O(n*m + n*x^2 + n*logn)
+disp(' ')
+disp('Run time of PIPthr_dtw_onlyxy:')
+tic
+[ ranking_PIPthr_dtw_onlyxy ] = SimRank_PIPthr_dtw_onlyxy( query_smooth,ts_smooth,PIPthr );
+toc
+%}
+
+%%%%%comparison - all-point dtw%%%%% - O(n*m^2 + n*logn)
+dtwwl=Inf;
+[ ranking_rawdata_dtw ] = SimRank_rawdata_dtw( query,ts, dtwwl);
+
+%sc accuracy
+
+disp(' ');
+accutmp=sum((fix((ranking_PIPthr_dtw(1:topNaccu)-1)/100)+1)==(fix((queryno-1)/100)+1))/topNaccu*100;%accuracy
+disp(['Top',num2str(topNaccu),' accuracy: ',num2str(accutmp),'% - PIPthr_dtw']);
+%accutmp=sum((fix((ranking_PIPthr_dtw_onlyxy(1:topNaccu)-1)/100)+1)==(fix((queryno-1)/100)+1))/topNaccu*100;%accuracy
+%disp(['Top',num2str(topNaccu),' accuracy: ',num2str(accutmp),'% - PIPthr_dtw_onlyxy']);
+accutmp=sum((fix((ranking_rawdata_dtw(1:topNaccu)-1)/100)+1)==(fix((queryno-1)/100)+1))/topNaccu*100;%accuracy
+disp(['Top',num2str(topNaccu),' accuracy: ',num2str(accutmp),'% - all-point DTW']);
+%}
