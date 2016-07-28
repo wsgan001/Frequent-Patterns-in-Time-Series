beee41061c2e86a5209657ad51b76491baf92b0a
Changfeng Liu <steven24liu@gmail.com>
2016-07-28 15:38:36 -0500

    Typical DTW only has X info

--- matlab/src/SIFTlike/SimRank_rawdata_dtw.m
+++ matlab/src/SIFTlike/SimRank_rawdata_dtw.m
@@ -15,8 +15,8 @@ addpath('../../lib/dynamic_time_warping_v2/dynamic_time_warping_v2.1');
 Dist=zeros(rnum,2);
 for i=1:rnum
     index=linspace(1,tslength,tslength)';
-    Dist(i,1)=dtw([index/tslength,query'],[index/tslength,dataset(i,:)'],wl);
-    %Dist(i,1)=dtw(query',dataset(i,:)',wl);%no X info
+    %Dist(i,1)=dtw([index/tslength,query'],[index/tslength,dataset(i,:)'],wl);
+    Dist(i,1)=dtw(query',dataset(i,:)',wl);%no X info
     %Dist(i,1)=dtw([index,query'],[index,dataset(i,:)'],wl);%no x-axis normalization
     Dist(i,2)=i;
 end
