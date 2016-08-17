0f580dfdf8aecea38d792fc216fdd4c8bb87d05b
Changfeng Liu <steven24liu@gmail.com>
2016-08-17 13:15:33 -0500

    add MVIPOnlyXYDist

---
+++ matlab/src/SIFTlike/MVIPOnlyXYDist.m
@@ -0,0 +1,38 @@
+function [ Dist ] = MVIPOnlyXYDist( query,dataset,PIPthr )
+
+addpath('/Users/Steven/Documents/GitHub/Frequent-Patterns-in-Time-Series/matlab/src/SIFTlike/getPIPs')
+addpath('/Users/Steven/Documents/GitHub/Frequent-Patterns-in-Time-Series/matlab/src/SIFTlike/matchingPIPs')
+addpath('/Users/Steven/Documents/GitHub/Frequent-Patterns-in-Time-Series/matlab/src/SIFTlike/PIPsIndicator')
+
+if nargin<3
+    PIPthr=0.15;
+end
+
+[rnum, ~]=size(dataset);
+
+%% PIPinfo
+PIPinfoQ = getPIPs_threshold(query, PIPthr);%query's info
+
+PIPinfoD=cell(rnum,1);%dataset's info
+for i=1:rnum
+    PIPinfoD{i,1}=getPIPs_threshold(dataset(i,:), PIPthr);
+end
+
+%% get indicators of PIPs
+[IndicatorQ,~] = getIndicator_onlyxy( query, PIPinfoQ );%query's info
+
+IndicatorD=cell(rnum,1);%dataset's info
+for i=1:rnum
+    [ tmp,~ ] = getIndicator_onlyxy( dataset(i,:), PIPinfoD{i,1} );
+    IndicatorD{i,1}=tmp;
+end
+
+%% dynamic computing
+Dist=zeros(rnum,1);
+for i=1:rnum
+    costmat=getCostmat(IndicatorQ,IndicatorD{i,1});
+    [~,tmp]=dtwMatch(costmat);
+    Dist(i,1)=tmp;
+end
+
+end
