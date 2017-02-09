019d469dd59864ac0045b296d033a29c202a73b5
Changfeng Liu <steven24liu@gmail.com>
2017-02-09 13:06:53 -0600

    add the new MVIP

--- matlab/src/adjustableWeights/PIPsIndicator/getCostmat.m
+++ matlab/src/adjustableWeights/PIPsIndicator/getCostmat.m
@@ -2,14 +2,14 @@ function [ Costmat ] = getCostmat( rIndicator,cIndicator )
 %rIndicator(i,:) stands for row i of Costmat
 %cIndicator(j,:) stands for column j of Costmat
 
-[rnum,~]=size(rIndicator);
+[rnum,dimension]=size(rIndicator);
 [cnum,~]=size(cIndicator);
 Costmat=zeros(rnum,cnum);
 
 for i=1:rnum
     for j=1:cnum
-        %Costmat(i,j)=sum(abs(rIndicator(i,:)-cIndicator(j,:)));%Manhattan distance, i.e. L-1 norm
-        Costmat(i,j)=norm(rIndicator(i,:)-cIndicator(j,:));%Euclidean distance
+        %Costmat(i,j)=norm(rIndicator(i,:)-cIndicator(j,:));%Euclidean distance
+        Costmat(i,j) = (norm(rIndicator(i,1:(dimension-1))-cIndicator(j,1:(dimension-1)))) * (rIndicator(i,end) + cIndicator(j,end)) / 2;% * average weights
     end
 end
 
