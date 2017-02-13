0d76212108a766b5e0e096d7d7e0a5aaa7d7539d
Changfeng Liu <steven24liu@gmail.com>
2017-02-13 11:15:58 -0600

    add seasonality

--- matlab/src/adjustableWeights/Seasonality.m
+++ matlab/src/adjustableWeights/Seasonality.m
@@ -1,6 +1,11 @@
 function [ seasonality ] = Seasonality( ts )
 % calculate one ts's seasonality score
+% seasonality = [frequency_max, frequency_max_amplitude, frequency_average, standard deviation]
 
-
+seasonality = zeros(1,4);
+ts_fdomain = abs(fft(ts));
+[seasonality(2),seasonality(1)] = max(ts_fdomain);
+seasonality(3) = (ts_fdomain .* (1:size(ts_fdomain,2))) / sum(ts_fdomain);
+seasonality(4) = std(ts_fdomain);
 
 end
